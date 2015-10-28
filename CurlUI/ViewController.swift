76b82794f33717c97b38708a24870d110f7899be
cemolcay <ccemolcay@gmail.com>
2015-10-28 10:57:49 -0700

    Initial Commit

---
+++ CurlUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CurlUI
+//
+//  Created by Cem Olcay on 28/10/15.
+//  Copyright © 2015 prototapp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
